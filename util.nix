34863921e0140eae1977c68b0bdea359603e0ccb
Christopher Fredén <christopher.freden@seal-software.com>
2018-02-12 17:03:47 +0100

    Added argument to provide a file to update in place

--- util.nix
+++ util.nix
@@ -8,9 +8,19 @@ lib.mapAttrs (name: script:
 ) {
   update-deps = writeScript "update-deps" ''
     export PATH=${jq}/bin:${nix-prefetch-git}/bin:$PATH
-    jq -r '. as $o | keys | map(.+" "+$o[.].url)[]' \
+    input=''${1-/dev/stdin}
+    if [ "$1" ]; then
+      output=$(mktemp)
+    else
+      output=/dev/stdout
+    fi
+    cat $input \
+      | jq -r '. as $o | keys | map(.+" "+$o[.].url)[]' \
       | while read k v; do echo "{\"$k\": $(nix-prefetch-git $v)}"; done \
-      | jq -s 'reduce .[] as $x ({}; . * $x)'
+      | jq -s 'reduce .[] as $x ({}; . * $x)' > $output
+    if [[ $input != /dev/stdin && $output != /dev/stdout ]]; then
+      mv $output $input
+    fi
   '';
 
   rtrav = writeScript "rtrav" ''
