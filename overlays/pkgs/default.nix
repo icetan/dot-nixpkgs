3498f3060dabb6aec97c455abc925f6e94cb4249
Christopher Fredén <me@icetan.org>
2019-02-01 13:31:02 +0100

    Add nix-lsp

--- overlays/pkgs/default.nix
+++ overlays/pkgs/default.nix
@@ -1,6 +1,10 @@
 self: super: with super;
 let
-   metals_version = "0.4.0";
+  metals_version = "0.4.0";
+  nightly = self.latest.rustChannels.nightly;
+  buildRustPackage = self.rustPlatform.buildRustPackage.override {
+    inherit (nightly) rustc cargo;
+  };
 in rec {
   docker-credential-helpers = callPackage (import ./docker-credential-helpers.nix) {};
   signal-cli = callPackage (import ./signal-cli.nix) {};
@@ -48,10 +52,17 @@ in rec {
     sha256 = "0zzm0s0anb7d6lnbn28njvj6by3jday2jvd3lf135vara4gi1s5r";
   })) {};
 
-  nix-lsp = import (fetchFromGitLab {
-    owner = "jD91mZM2";
-    repo = "nix-lsp";
-    rev = "f0ad31d24e350adf52a3689812cecbcf6406d125";
-    sha256 = "05l44wql2i5xa5ni9rqanqgm3iv32wkdi1acck0mbjrr5pkrzqqi";
-  });
+  nix-lsp = buildRustPackage {
+    name = "nix-lsp";
+    src = fetchFromGitLab {
+      owner = "jD91mZM2";
+      repo = "nix-lsp";
+      rev = "0db95183edf2f6be9bfff23e4a59a721d60628c7";
+      sha256 = "1b5m3ghb4a5pzn4nclzssqs9s3j4ks5nag0nrvlaz8r2ms2pv5qi";
+    };
+    cargoSha256 = "1n01dzm0ngy1kn42xdmkcc83cxkk9552l5spd5a4ack4s4rdlrm2";
+
+    # See https://github.com/NixOS/nixpkgs/issues/25863#issuecomment-302633494
+    RUSTFLAGS="-L ${nightly.rust}/lib/rustlib/x86_64-unknown-linux-gnu/lib/";
+  };
 }
