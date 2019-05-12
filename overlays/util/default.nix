48aeb2a802a1e89117aea28883e783ca360f34d4
Christopher Fredén <me@icetan.org>
2019-05-12 13:47:07 +0200

    Update util update-deps to output fetchGit compatible JSON

--- overlays/util/default.nix
+++ overlays/util/default.nix
@@ -13,6 +13,7 @@ in rec {
     #!${dash}/bin/dash
     set -e
 
+    GIT_JSON_FILTER='with_entries(select(.key | in({"url":0,"rev":0,"date":0})))'
     export PATH=${jq}/bin:${nix-prefetch-git}/bin:$PATH
     input=''${1-/dev/stdin}
     if test -n "$1"; then
@@ -22,7 +23,7 @@ in rec {
     fi
     cat $input \
       | jq -r '. as $o | keys | map(.+" "+$o[.].url)[]' \
-      | while read k v; do echo "{\"$k\": $(nix-prefetch-git $v)}"; done \
+      | while read k v; do echo "{\"$k\": $(nix-prefetch-git $v | jq -r "$GIT_JSON_FILTER")}"; done \
       | jq -s 'reduce .[] as $x ({}; . * $x)' > $output
     if { test $input != /dev/stdin && test $output != /dev/stdout; }; then
       mv $output $input
